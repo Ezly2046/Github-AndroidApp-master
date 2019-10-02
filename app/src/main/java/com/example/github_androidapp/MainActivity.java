0e4e41d354f7bd4efe087ac06df64cf0b9745396
Ezly2406 <jlazarooooo@gmail.com>
2019-10-02 10:47:40 +0800

    fourth commit

--- app/src/main/java/com/example/github_androidapp/MainActivity.java
+++ app/src/main/java/com/example/github_androidapp/MainActivity.java
@@ -3,10 +3,12 @@ package com.example.github_androidapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     ImageView imgView;
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
