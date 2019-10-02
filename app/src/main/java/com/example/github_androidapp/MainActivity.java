0430a9bceb274eef979d64cefd0f796e00d45e0e
Ezly2406 <jlazarooooo@gmail.com>
2019-10-02 10:44:17 +0800

    third commit

--- app/src/main/java/com/example/github_androidapp/MainActivity.java
+++ app/src/main/java/com/example/github_androidapp/MainActivity.java
@@ -3,9 +3,10 @@ package com.example.github_androidapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageView imgView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
